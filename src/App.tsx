3c6802d8d4754bd85139ee548a2ee8fa28468a76
Daro <dario.palminio@gmail.com>
2021-05-16 13:55:43 -0400

    Add useState case

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import {FC} from "react";
+import { FC } from "react";
 import UserFormWithReducer from "./components/UserFormWithReducer";
 import UserFormWithAtom from "./components/UserFormWithAtom";
 import UserFormWithCustomHook from "./components/UserFormWithCustomHook";
+import UserFormWithUseState from "./components/UserFormWithUseState";
 import "./App.css";
 
-
 const App: FC = () => {
-
   return (
     <div className="App">
       <h1>State management examples...</h1>
 
-     <UserFormWithReducer/>
-     
-     <UserFormWithAtom/>
+      <UserFormWithUseState />
 
-     <UserFormWithCustomHook/>
+      <UserFormWithReducer />
 
+      <UserFormWithAtom />
+
+      <UserFormWithCustomHook />
     </div>
   );
 };
---
+++ src/components/UserFormWithUseState.tsx
@@ -0,0 +1,42 @@
+import React, { FC, useState } from "react";
+
+
+const UserFormWithUseState: FC = () => {
+ 
+  const [user, setUser] = useState('');
+  const [email, setEmail] = useState('');
+
+  let refFormNameInput = React.createRef<HTMLInputElement>();
+  let refFormEmailInput = React.createRef<HTMLInputElement>();
+
+  const styleColor = {  backgroundColor: "#7c78a1"};
+  
+  const updateUser = (): void => {
+    if (refFormNameInput.current) {
+        setUser(refFormNameInput.current.value);
+    }
+    if (refFormEmailInput.current) {
+        setEmail(refFormEmailInput.current.value);
+    }
+  };
+
+  return (
+    <div className="UserForm" style={styleColor}>
+      <h2>Using useState</h2>
+      <div id="form-container-useState">
+        <div id="user-form-useState">
+          <label>name:</label>
+          <input className="input-test-useState" ref={refFormNameInput} id="form-name-input-useState" aria-label="form-name-input-reducer"></input>
+        </div>
+        <div>
+          <label>email:</label>
+          <input ref={refFormEmailInput} id="form-name-email-useState"></input>
+        </div>
+        <button onClick={() => updateUser()}>Update user</button>
+      </div>
+      <div>{` User name: ${user} Email: ${email}`}</div>
+    </div>
+  );
+};
+
+export default UserFormWithUseState;
