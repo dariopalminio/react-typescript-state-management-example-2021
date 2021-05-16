e8518ea2940e851c87a7303a23bb92b0e7a02718
Daro <dario.palminio@gmail.com>
2021-05-16 14:24:59 -0400

    Add paragraphs in components

--- src/components/UserFormWithReducer.tsx
+++ src/components/UserFormWithReducer.tsx
@@ -1,16 +1,18 @@
 import React, { FC, useReducer } from "react";
-import { userReducer, userInitialState, ActionType } from "../states/userReducer";
-
+import {
+  userReducer,
+  userInitialState,
+  ActionType,
+} from "../states/userReducer";
 
 const UserFormWithReducer: FC = () => {
- 
   const [userState, dispatch] = useReducer(userReducer, userInitialState);
 
   let refFormNameInput = React.createRef<HTMLInputElement>();
   let refFormEmailInput = React.createRef<HTMLInputElement>();
 
-  const styleColor = {  backgroundColor: "#7c9ea1"};
-  
+  const styleColor = { backgroundColor: "#7c9ea1" };
+
   const updateUser = (): void => {
     if (refFormNameInput.current && refFormEmailInput.current) {
       const newUser = {
@@ -24,10 +26,27 @@ const UserFormWithReducer: FC = () => {
   return (
     <div className="UserForm" style={styleColor}>
       <h2>Using userReducer from react hook</h2>
+      <p>
+        If you move past managing a single primitive (i.e. a string, integer, or
+        boolean) and instead must manage a complex object (e.g. with objects,
+        arrays and additional primitives), you may be better of using
+        useReducer. We also use it to manage logic in one part and separate view
+        from logic. Another important advantage is an improved developer
+        experience: With one code block (the reducer function) managing multiple
+        transitions of one state object, it is far easier to debug your logic if
+        anything should go wrong. Another great advantage of having all state
+        transitions neatly organized into one reducer function is the ability to
+        export the reducer for unit tests.
+      </p>
       <div id="form-container">
         <div id="user-form">
           <label>name:</label>
-          <input className="input-test" ref={refFormNameInput} id="form-name-input-reducer" aria-label="form-name-input-reducer"></input>
+          <input
+            className="input-test"
+            ref={refFormNameInput}
+            id="form-name-input-reducer"
+            aria-label="form-name-input-reducer"
+          ></input>
         </div>
         <div>
           <label>email:</label>
--- src/components/UserFormWithUseState.tsx
+++ src/components/UserFormWithUseState.tsx
@@ -1,32 +1,40 @@
 import React, { FC, useState } from "react";
 
-
 const UserFormWithUseState: FC = () => {
- 
-  const [user, setUser] = useState('');
-  const [email, setEmail] = useState('');
+  const [user, setUser] = useState("");
+  const [email, setEmail] = useState("");
 
   let refFormNameInput = React.createRef<HTMLInputElement>();
   let refFormEmailInput = React.createRef<HTMLInputElement>();
 
-  const styleColor = {  backgroundColor: "#7c78a1"};
-  
+  const styleColor = { backgroundColor: "#7c78a1" };
+
   const updateUser = (): void => {
     if (refFormNameInput.current) {
-        setUser(refFormNameInput.current.value);
+      setUser(refFormNameInput.current.value);
     }
     if (refFormEmailInput.current) {
-        setEmail(refFormEmailInput.current.value);
+      setEmail(refFormEmailInput.current.value);
     }
   };
 
   return (
     <div className="UserForm" style={styleColor}>
       <h2>Using useState</h2>
+      <p>
+        The useState is a Hook (function) that allows you to have state
+        variables in functional components. Use useState for store values
+        whenever you manage a JS primitive (i.e. a string, integer, or boolean).
+      </p>
       <div id="form-container-useState">
         <div id="user-form-useState">
           <label>name:</label>
-          <input className="input-test-useState" ref={refFormNameInput} id="form-name-input-useState" aria-label="form-name-input-reducer"></input>
+          <input
+            className="input-test-useState"
+            ref={refFormNameInput}
+            id="form-name-input-useState"
+            aria-label="form-name-input-reducer"
+          ></input>
         </div>
         <div>
           <label>email:</label>
